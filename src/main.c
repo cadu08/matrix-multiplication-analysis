46da4e2058852b1e7571891b35b131dc9b25d178
cadu08 <carlos.ceaf08@gmail.com>
2026-05-02 23:24:05 -0300

    Add timing utility with clock_gettime

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "utils.h"
 
 int main() {
-    printf("Matrix multiplication project ready.\n");
+    volatile long sum = 0;
+
+    double start = get_time();
+
+    for (long i = 0; i < 100000000; i++) {
+        sum += i;
+    }
+
+    double end = get_time();
+
+    printf("Sum: %ld\n", sum);
+    printf("Elapsed time: %f seconds\n", end - start);
+
     return 0;
 }
---
+++ src/utils.c
@@ -0,0 +1,8 @@
+#include <time.h>
+#include "utils.h"
+
+double get_time() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec * 1e-9;
+}
