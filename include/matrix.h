025390e8a55afc36b4cc2f1f850fc1f1b929bd78
cadu08 <carlos.ceaf08@gmail.com>
2026-05-04 21:30:29 -0300

    Add matrix file output utility

--- include/matrix.h
+++ include/matrix.h
@@ -3,5 +3,6 @@
 
 void fill_random_matrix(int *matrix, int n);
 void print_matrix(int *matrix, int n);
+void write_matrix_to_file(const char *filename, int *matrix, int n);
 
 #endif
--- src/matrix.c
+++ src/matrix.c
@@ -16,3 +16,21 @@ void print_matrix(int *matrix, int n) {
         printf("\n");
     }
 }
+
+void write_matrix_to_file(const char *filename, int *matrix, int n) {
+    FILE *f = fopen(filename, "w");
+
+    if (f == NULL) {
+        printf("Error opening file: %s\n", filename);
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            fprintf(f, "%d ", matrix[i * n + j]);
+        }
+        fprintf(f, "\n");
+    }
+
+    fclose(f);
+}
