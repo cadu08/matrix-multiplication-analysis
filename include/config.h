4ce43d698ed5dcaaa6a6cd60e1c37fb366c330cb
cadu08 <carlos.ceaf08@gmail.com>
2026-05-04 21:16:13 -0300

    Add experiment configuration constants

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define NUM_SIZES 5
+#define NUM_RUNS 10
+#define RANDOM_SEED 42
+
+static const int MATRIX_SIZES[NUM_SIZES] = {
+    64, 128, 256, 512, 1024
+};
+
+#endif
